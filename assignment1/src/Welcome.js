c15631110f8489f9b35f056c4e61fe9d42404d76
lilly <kokkiligaddalillygrace@gmail.com>
2022-02-09 08:52:59 +0530

    new project

---
+++ assignment1/src/Welcome.js
@@ -0,0 +1,23 @@
+import Image from './image/lizard.png';
+import './styles.css';
+let cmd1='SHARE'
+let cmd2='LEARN MORE'
+
+function Welcome  () {
+     
+    return (
+    <>
+    <img src={Image} />
+    <h1>LIZARD</h1>
+    <p>Lizards are a widespread group of squamate reptiles,with over 6,000 species,ranging across 
+        all continents except Antartica</p>
+    <h2>{cmd1+'  '+cmd2}</h2>  
+    
+
+    
+    </>
+        
+    );
+}
+
+export default Welcome;
---
+++ assignment1/src/index.js
@@ -0,0 +1,14 @@
+//importing the dependencies
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './styles.css';
+import Message from './Welcome.js';
+
+//display
+ReactDOM.render(
+<>
+<Message/>
+</>,
+document.getElementById('root')
+
+)
